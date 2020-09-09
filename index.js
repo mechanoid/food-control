e1c983aaea6753b422b8144f64bf8dabeb807e98
Falk Hoppe <falkhoppe81@gmail.com>
2020-09-09 10:22:32 +0200

    initial proto

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
+
+import 'pug'
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const app = express()
 app.use(helmet())
 app.use(morgan('combined'))
 
+app.use('/assets', express.static('./assets'))
+app.set('view engine', 'pug')
+
 export default config => {
-  console.log(config)
+  console.log('config', config)
+
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
+
+  app.get('/defecations/new', (req, res) => {
+    res.render('defecations/new', { date: new Date() })
+  })
+
+  app.post('/defecations', urlencodedParser, (req, res) => {
+    const { quality, date } = req.body
+    console.log(quality, date)
+    res.redirect('/')
+  })
+
   return app
 }
